2911b81e8c88e5000fed0dd44ea49b2fb4814247
Shiza Riaz <theev2121@gmail.com>
2025-12-02 15:03:59 +0500

    Add files via upload
    How it works:
    
    User enters a number (integer or float depending on your code).
    
    The program multiplies the number by itself.
    
    It prints the result as the square of the given number.
    
    This program is great for practicing basic calculations and understanding how input/output works in C.

---
+++ square.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   printf("Enter the value of n = \n");
+   scanf("%d", &n);
+   int square  = n * n;
+
+   printf("The square of n is = %d\n", square);
+   return 0;
+}
